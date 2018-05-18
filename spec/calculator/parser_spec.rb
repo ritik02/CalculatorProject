b9ae0896bc26c4089ce8b3f9ddffa5e3d6a3a20d
Ritik Verma <ritikverma@Ritiks-MacBook-Pro.local>
2018-05-18 23:17:51 +0530

    Add before method in Rspec classes for refactoring

--- spec/calculator/parser_spec.rb
+++ spec/calculator/parser_spec.rb
@@ -1,25 +1,25 @@
 RSpec.describe Parser do
   context "When checking for proper parsing of command string" do
-
+    before(:each) do
+      @object = Parser.new
+    end
+    
     it "parser object should exist" do
-      object = Parser.new
-      expect(object).not_to be_nil
+      expect(@object).not_to be_nil
     end
 
     it "should return no error when input is valid" do
-      object = Parser.new
       input = "add 3.4"
       input1 = "cancel"
-      error1, command_array1 = object.parse(input1)
-      error, command_array = object.parse(input)
+      error1, command_array1 = @object.parse(input1)
+      error, command_array = @object.parse(input)
       expect(error).to eql('')
       expect(error1).to eql('')
     end
 
     it "should return properly splitted arguments" do
-      object = Parser.new
       input = "multiply 12"
-      error, command_array = object.parse(input)
+      error, command_array = @object.parse(input)
       size = command_array.length
       expect(size).to eql(2)
       expect(command_array[0]).to eql("multiply")
--- spec/calculator/router_spec.rb
+++ spec/calculator/router_spec.rb
@@ -1,104 +1,92 @@
 RSpec.describe Router do
   context "When checking for proper routing of parsed commands" do
-
+    before(:each) do
+      @routerobject = Router.new
+      @calculatorobject = CalculatorHelper.new
+    end
+    
     it "router object should exist" do
-      object = Router.new
-      expect(object).not_to be_nil
+      expect(@routerobject).not_to be_nil
     end
 
     it "should route add command properly" do
-      object = Router.new
-      calculatorobject = CalculatorHelper.new
       command_array = ['add', 4]
-      result = object.route(command_array,calculatorobject)
+      result = @routerobject.route(command_array,@calculatorobject)
       expect(result).to eql(4.0)
     end
 
     it "should route subtract command properly" do
-      object = Router.new
-      calculatorobject = CalculatorHelper.new
       command_array = ['subtract', 2]
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, @calculatorobject)
       expect(result).to eql(-2.0)
     end
     
     it "should route exit command properly" do
-      object = Router.new
-      calculatorobject = CalculatorHelper.new
       command_array = ['exit']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, @calculatorobject)
       expect(result).to eql("Program Terminated")
     end
 
     it "should route multiply command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(2)
       command_array = ['multiply',4]
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(8.0)
     end
 
     it "should route divide command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(4)
       command_array = ['divide', 2]
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(2.0)
     end
 
     it "should route neg command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(2)
       command_array = ['neg']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(-2.0)
     end
 
     it "should route abs command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(-2.0)
       command_array = ['abs']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(2.0)
     end
 
     it "should route sqr command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(2)
       command_array = ['sqr']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(4.0)
     end
 
     it "should route cube command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(-4)
       command_array = ['cube']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(-64.0)
     end
 
     it "should route sqrt command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(64.0)
       command_array = ['sqrt']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(8.0)
     end
 
     it "should route cubert command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(8.0)
       command_array = ['cubert']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(2.0)
     end
     
     it "should route cancel command properly" do
-      object = Router.new
       calculatorobject = CalculatorHelper.new(5)
       command_array = ['cancel']
-      result = object.route(command_array, calculatorobject)
+      result = @routerobject.route(command_array, calculatorobject)
       expect(result).to eql(0.0)
     end
     
